6c01c5b261beaaab617d79e0cf6be4797d9367b0
HyunJinNo <88878230+HyunJinNo@users.noreply.github.com>
2024-02-19 22:36:07 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 120 ms, Memory: 13264 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt() // 1 <= n <= 1000
+    val arr = IntArray(n + 1) { 0 }
+    arr[0] = 1
+    arr[1] = 1
+    for (i in 2..n) {
+        arr[i] = (arr[i - 1] + (arr[i - 2] * 2)) % 10007
+    }
+    println(arr[n])
+}
