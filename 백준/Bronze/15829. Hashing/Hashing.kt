9865fe6422001b501a60e2a00d5d6876fe9839f2
HyunJinNo <88878230+HyunJinNo@users.noreply.github.com>
2024-02-21 15:55:46 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 13260 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -1,14 +1,16 @@
 import java.util.*
-import kotlin.math.pow
 
 fun main() {
     val sc = Scanner(System.`in`)
     val L = sc.nextInt() // 1 <= L <= 50
     val str = sc.next()
-    var answer = 0
+    var answer = 0L
+    var r = 1L
 
     for (i in 0 until L) {
-        answer += (str[i].code - 96) * (31.0.pow(i).toInt()) % 1234567891
+        answer += (((str[i].code - 96).toLong() * r) % 1234567891L)
+        answer %= 1234567891L
+        r = (r * 31L) % 1234567891L
     }
     println(answer)
 }
