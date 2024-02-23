0bbc31540a4216e553193bd274bfa9115888cdb4
HyunJinNo <88878230+HyunJinNo@users.noreply.github.com>
2024-02-23 23:01:41 +0900

    [Silver II] Title: N과 M (10), Time: 172 ms, Memory: 23944 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.kt
@@ -0,0 +1,44 @@
+import java.io.*
+
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+lateinit var arr: IntArray
+val list = mutableListOf<Int>()
+val printed = mutableSetOf<String>()
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val temp = br.readLine().split(" ").map { it.toInt() }
+
+    val n = temp[0]
+    val m = temp[1]
+
+    arr = br.readLine().split(" ").map { it.toInt() }.toIntArray().sortedArray()
+    br.close()
+
+    solve(m, 0)
+    bw.flush()
+    bw.close()
+}
+
+fun solve(depth: Int, index: Int) {
+    if (depth == 0) {
+        val result = buildString {
+            for (x in list) {
+                append("$x ")
+            }
+            append("\n")
+        }
+
+        if (!printed.contains(result)) {
+            bw.write(result)
+            printed.add(result)
+        }
+        return
+    }
+
+    for (i in index..(arr.size - 1)) {
+        list.add(arr[i])
+        solve(depth - 1, i + 1)
+        list.removeLast()
+    }
+}
