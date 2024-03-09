366f65f45b38ee30b82775bddc500ef76ec60ca5
HyunJinNo <88878230+HyunJinNo@users.noreply.github.com>
2024-03-09 21:18:33 +0900

    [Gold II] Title: 반도체 설계, Time: 316 ms, Memory: 27948 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.kt
@@ -0,0 +1,35 @@
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = br.readLine().toInt() // 포트의 개수, 1 <= n <= 40,000
+    val list = br.readLine().split(" ").map { it.toInt() }
+    br.close()
+
+    val lis = mutableListOf<Int>()
+    lis.add(list[0])
+    for (i in 1..(n - 1)) {
+        when {
+            list[i] < lis.first() -> lis[0] = list[i]
+            list[i] > lis.last() -> lis.add(list[i])
+            else -> {
+                var left = 0
+                var right = lis.lastIndex
+                while (left + 1 < right) {
+                    val mid = (left + right) / 2
+                    if (list[i] < lis[mid]) {
+                        right = mid
+                    } else {
+                        left = mid
+                    }
+                }
+                lis[right] = list[i]
+            }
+        }
+    }
+
+    bw.write("${lis.size}")
+    bw.flush()
+    bw.close()
+}
