a2c2a0d461360526ff554ad4a693950e429eb97e
HyunJinNo <88878230+HyunJinNo@users.noreply.github.com>
2024-03-22 09:49:55 +0900

    [Silver V] Title: 돌 게임, Time: 200 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -0,0 +1,9 @@
+const path = (process.platform === "linux") ? "/dev/stdin" : "input.txt";
+const input = require('fs').readFileSync(path).toString();
+const n = Number(input); // 돌의 개수, 1 <= n <= 1,000
+
+if (n % 2 == 1) {
+  console.log("SK");
+} else {
+  console.log("CY");
+}
