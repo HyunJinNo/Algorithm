0fdcc3245f12a41d9eaa35d5b1c2063b5f6d0a67
HyunJinNo <88878230+HyunJinNo@users.noreply.github.com>
2024-04-05 09:23:13 +0900

    [Gold V] Title: 무한 수열, Time: 128 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Gold/1351. 무한 수열/무한 수열.ts
@@ -0,0 +1,27 @@
+const path: string =
+  process.platform === "linux" ? "/dev/stdin" : "./TypeScript/src/input.txt";
+const input: string = require("fs").readFileSync(path).toString().trim();
+
+// 0 <= n <= 1_000_000_000_000
+// 2 <= p <= 1_000_000_000
+// 2 <= q <= 1_000_000_000
+const [n, p, q] = input.split(" ").map(Number);
+const map = new Map<number, number>();
+map.set(0, 1);
+
+/**
+ * A[i]를 구하는 함수
+ * @param i 인덱스
+ * @returns A[i] 값
+ */
+const solve = (i: number): number => {
+  if (map.get(i) !== undefined) {
+    return map.get(i)!;
+  } else {
+    const result = solve(Math.floor(i / p)) + solve(Math.floor(i / q));
+    map.set(i, result);
+    return result;
+  }
+};
+
+console.log(solve(n));
