03dfbdd6d092f0d8c3f65c33ee1bcbab4f96995f
HyunJinNo <88878230+HyunJinNo@users.noreply.github.com>
2024-05-04 10:48:46 +0900

    [Gold III] Title: 줄 세우기, Time: 444 ms, Memory: 42224 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.js
@@ -0,0 +1,256 @@
+class DataNode {
+  constructor(value, prev, next) {
+    this._value = value;
+    this._prev = prev;
+    this._next = next;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  set value(value) {
+    this._value = value;
+  }
+
+  get prev() {
+    return this._prev;
+  }
+
+  set prev(prev) {
+    this._prev = prev;
+  }
+
+  get next() {
+    return this._next;
+  }
+
+  set next(next) {
+    this._next = next;
+  }
+}
+
+/**
+ * A linear collection that supports element insertion and removal
+ * at both ends. The name deque is short for "double ended queue" and
+ * is usually pronounced "deck".
+ */
+class Deque {
+  constructor() {
+    this._head = null;
+    this._tail = null;
+    this._size = 0;
+  }
+
+  /**
+   * Retrieves, but does not remove, the first element of this deque.
+   * This method differs from peekFirst( ) only in that it throws an exception
+   * if this deque is empty.
+   *
+   * Time Complexity: O(1)
+   * @returns the head of this deque
+   */
+  getFirst = () => {
+    if (this._size === 0) {
+      throw Error("The deque is empty.");
+    } else {
+      return this._head.value;
+    }
+  };
+
+  /**
+   * Retrieves, but does not remove, the last element of this deque.
+   * This method differs from peekLast( ) only in that it throws an exception
+   * if this deque is empty.
+   *
+   * Time Complexity: O(1)
+   * @returns the tail of this deque
+   */
+  getLast = () => {
+    if (this._size === 0) {
+      throw new Error("The deque is empty.");
+    } else {
+      return this._tail.value;
+    }
+  };
+
+  /**
+   * Retrieves, but does not remove, the first element of this deque,
+   * or returns null if this deque is empty.
+   *
+   * Time Complexity: O(1)
+   * @returns the head of this deque, or null if this deque is empty
+   */
+  peekFirst = () => {
+    if (this._size === 0) {
+      return null;
+    } else {
+      return this._head.value;
+    }
+  };
+
+  /**
+   * Retrieves, but does not remove, the last element of this deque,
+   * or returns null if this deque is empty.
+   *
+   * Time Complexity: O(1)
+   * @returns the tail of this deque, or null if this deque is empty
+   */
+  peekLast = () => {
+    if (this._size === 0) {
+      return null;
+    } else {
+      return this._tail.value;
+    }
+  };
+
+  /**
+   * Returns the number of elements in this deque.
+   *
+   * Time Complexity: O(1)
+   * @returns the number of elements in this deque
+   */
+  size = () => {
+    return this._size;
+  };
+
+  /**
+   * Returns true if this deque contains no elements.
+   *
+   * Time Complexity: O(1)
+   * @returns true if this deque contains no elements
+   */
+  isEmpty = () => (this._size === 0 ? true : false);
+
+  /**
+   * Inserts the specified element at the front of this deque.
+   *
+   * Time Complexity: O(1)
+   * @param value the element to add
+   */
+  addFirst = (value) => {
+    if (this._size === 0) {
+      const node = new DataNode(value);
+      this._head = node;
+      this._tail = node;
+    } else {
+      const node = new DataNode(value, undefined, this._head);
+      this._head.prev = node;
+      this._head = node;
+    }
+    this._size++;
+  };
+
+  /**
+   * Inserts the specified element at the end of this deque.
+   *
+   * Time Complexity: O(1)
+   * @param value the element to add
+   */
+  addLast = (value) => {
+    if (this._size === 0) {
+      const node = new DataNode(value);
+      this._head = node;
+      this._tail = node;
+    } else {
+      const node = new DataNode(value, this._tail);
+      this._tail.next = node;
+      this._tail = node;
+    }
+    this._size++;
+  };
+
+  /**
+   * Retrieves and removes the first element of this deque.
+   * This method throws an exception if this deque is empty.
+   *
+   * Time Complexity: O(1)
+   * @returns the head of this deque
+   */
+  removeFirst = () => {
+    if (this._size === 0) {
+      throw new Error("The deque is empty.");
+    } else {
+      const node = this._head;
+      this._head = this._head.next;
+      this._size--;
+      return node.value;
+    }
+  };
+
+  /**
+   * Retrieves and removes the last element of this deque.
+   * This methods throws an exception if this deque is empty.
+   *
+   * Time Complexity: O(1)
+   * @returns the tail of this deque
+   */
+  removeLast = () => {
+    if (this._size === 0) {
+      throw new Error("The deque is empty");
+    } else {
+      const node = this._tail;
+      this._tail = this._tail.prev;
+      this._size--;
+      return node.value;
+    }
+  };
+
+  /**
+   * Removes all of the elements from this deque.
+   * The deque will be empty after this call returns.
+   *
+   * Time Complexity: O(1)
+   */
+  clear = () => {
+    this._head = null;
+    this._tail = null;
+    this._size = 0;
+  };
+}
+
+const path = process.platform === "linux" ? "/dev/stdin" : "./src/input.txt";
+const input = require("fs").readFileSync(path).toString().split("\n");
+
+// N: 학생의 수, 1 <= N <= 32_000
+// M: 키를 비교한 횟수, 1 <= M <= 100_000
+const [N, M] = input[0].split(" ").map(Number);
+const inDegree = new Int32Array(N + 1).fill(0); // 진입 차수
+const queue = new Deque();
+const graph = new Map();
+let answer = "";
+
+for (let i = 1; i <= N; i++) {
+  graph.set(i, []);
+}
+
+for (let i = 1; i <= M; i++) {
+  // 1 <= A, B <= N
+  const [A, B] = input[i].split(" ").map(Number);
+  graph.get(A)?.push(B);
+  inDegree[B]++;
+}
+
+for (let i = 1; i <= N; i++) {
+  // 진입 차수가 0인 노드들 큐에 삽입한다.
+  if (inDegree[i] === 0) {
+    queue.addLast(i);
+  }
+}
+
+// 위상 정렬이 완전히 수행된다면 정확히 N개의 노드를 방문한다.
+for (let iter = 1; iter <= N; iter++) {
+  const current = queue.removeFirst();
+  answer += `${current} `;
+
+  const destinations = graph.get(current);
+  const length = destinations.length;
+  for (let i = 0; i < length; i++) {
+    inDegree[destinations[i]]--;
+    if (inDegree[destinations[i]] === 0) {
+      queue.addLast(destinations[i]);
+    }
+  }
+}
+
+console.log(answer);
