7907cf410b74212aea9ef8224f4bb54a05907ae8
노현진 <nhj9831@naver.com>
2024-07-19 09:14:18 +0900

    [Bronze V] Title: 이상한 기호, Time: 120 ms, Memory: 9332 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.js
@@ -0,0 +1,6 @@
+const path =
+  process.platform === "linux" ? "/dev/stdin" : "./algorithm/input.txt";
+const input = require("fs").readFileSync(path).toString();
+
+const [A, B] = input.trim().split(" ").map(Number);
+console.log((A + B) * (A - B));
