2fb11461fb1e102f486ccabca801c94fb1a3967f
노현진 <nhj9831@naver.com>
2024-08-21 09:15:19 +0900

    [Bronze IV] Title: 주사위, Time: 136 ms, Memory: 11092 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.js
@@ -0,0 +1,9 @@
+const path =
+  process.platform === "linux" ? "/dev/stdin" : "./algorithm/input.txt";
+const input = require("fs").readFileSync(path).toString().split("\n");
+const T = Number(input[0]); // 테스트 케이스의 개수
+
+for (let i = 1; i <= T; i++) {
+  const [a, b] = input[i].trim().split(" ").map(Number);
+  console.log(`Case ${i}: ${a + b}`);
+}
