ade1c881891f0f1e52e3ef34ab1851be4d0a38d1
노현진 <nhj9831@naver.com>
2024-09-11 09:19:37 +0900

    [Bronze V] Title: A+B - 9, Time: 100 ms, Memory: 9388 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.js
@@ -0,0 +1,5 @@
+const path =
+  process.platform === "linux" ? "/dev/stdin" : "./algorithm/input.txt";
+const input = require("fs").readFileSync(path).toString();
+const [A, B] = input.trim().split(" ").map(BigInt);
+console.log((A + B).toString());
