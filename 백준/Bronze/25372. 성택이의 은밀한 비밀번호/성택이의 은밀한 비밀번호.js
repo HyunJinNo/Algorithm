649c7a9b8e9f20932777665d30682204bf1e5c87
노현진 <nhj9831@naver.com>
2024-10-11 09:18:40 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 136 ms, Memory: 10476 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.js
@@ -0,0 +1,13 @@
+const path =
+  process.platform === "linux" ? "/dev/stdin" : "./algorithm/input.txt";
+const input = require("fs").readFileSync(path).toString().split("\n");
+const N = Number(input[0]);
+
+for (let i = 1; i <= N; i++) {
+  const length = input[i].trim().length;
+  if (length >= 6 && length <= 9) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
+}
