25606e033498ed612fc4321a443cf0f2b1efd650
노현진 <nhj9831@naver.com>
2025-01-11 10:29:50 +0900

    [Bronze V] Title: Java Warriors, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.js
@@ -0,0 +1,5 @@
+const path =
+  process.platform === "linux" ? "/dev/stdin" : "./algorithm/input.txt";
+const input = require("fs").readFileSync(path).toString();
+const n = Number(input);
+console.log(n * 4000);
