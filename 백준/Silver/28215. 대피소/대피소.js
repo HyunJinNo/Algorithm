46dd7eb408ed57f7a3acc449151c4c0e4ff87737
노현진 <nhj9831@naver.com>
2025-04-21 11:18:50 +0900

    [Silver IV] Title: 대피소, Time: 180 ms, Memory: 10676 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.js
+++ 백준/Silver/28215. 대피소/대피소.js
@@ -6,53 +6,71 @@ const input = require("fs").readFileSync(path, "utf-8").toString().split("\n");
 const [N, K] = input[0].split(" ").map(Number);
 const pos = [];
 const distance = Array.from({ length: N }, () => Array(N).fill(0));
-let answer = 100_001;
 
 for (let i = 1; i <= N; i++) {
   pos.push(input[i].split(" ").map(Number));
 }
 
 for (let i = 0; i < N; i++) {
-  for (let j = i + 1; j < N; j++) {
-    const temp =
+  for (let j = 0; j < N; j++) {
+    distance[i][j] =
       Math.abs(pos[i][0] - pos[j][0]) + Math.abs(pos[i][1] - pos[j][1]);
-    distance[i][j] = temp;
-    distance[j][i] = temp;
   }
 }
 
-/**
- * @param {number} index
- * @param {number[]} shelter
- */
-const solution = (index, shelter) => {
-  if (shelter.length === K) {
-    let result = 0;
+let answer = Number.MAX_SAFE_INTEGER;
 
+switch (K) {
+  case 1:
     for (let i = 0; i < N; i++) {
-      if (!shelter.includes(i)) {
-        let temp = 100_001;
+      let result = 0;
+      for (let j = 0; j < N; j++) {
+        if (i === j) {
+          continue;
+        }
+        result = Math.max(result, distance[i][j]);
+      }
 
-        for (let j = 0; j < K; j++) {
-          temp = Math.min(temp, distance[i][shelter[j]]);
+      answer = Math.min(answer, result);
+    }
+    break;
+  case 2:
+    for (let i = 0; i < N; i++) {
+      for (let j = i + 1; j < N; j++) {
+        let result = 0;
+        for (let k = 0; k < N; k++) {
+          if (k === i || k === j) {
+            continue;
+          }
+          result = Math.max(result, Math.min(distance[k][i], distance[k][j]));
         }
 
-        result = Math.max(result, temp);
+        answer = Math.min(answer, result);
       }
     }
+    break;
+  case 3:
+    for (let i = 0; i < N; i++) {
+      for (let j = i + 1; j < N; j++) {
+        for (let k = j + 1; k < N; k++) {
+          let result = 0;
+          for (let l = 0; l < N; l++) {
+            if (l === i || l === j || l === k) {
+              continue;
+            }
+            result = Math.max(
+              result,
+              Math.min(distance[l][i], distance[l][j], distance[l][k]),
+            );
+          }
 
-    answer = Math.min(answer, result);
-    return;
-  } else if (index >= N) {
-    return;
-  }
+          answer = Math.min(answer, result);
+        }
+      }
+    }
+    break;
+  default:
+    break;
+}
 
-  solution(index + 1, shelter);
-
-  shelter.push(index);
-  solution(index + 1, shelter);
-  shelter.pop();
-};
-
-solution(0, []);
 console.log(answer);
