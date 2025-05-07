241094ccfb3c1f7468700723ce906291e1b2b0cd
노현진 <nhj9831@naver.com>
2025-05-07 15:01:15 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -1,2 +1,17 @@
--- 코드를 입력하세요
-select PT_NAME, PT_NO, GEND_CD, AGE, case when TLNO is null then "NONE" else TLNO end as TLNO from PATIENT where GEND_CD = "W" and AGE <= 12 order by AGE desc, PT_NAME asc;
+select
+    PT_NAME,
+    PT_NO,
+    GEND_CD,
+    AGE,
+    case
+        when TLNO is null then "NONE"
+        else TLNO
+    end as TLNO
+from
+    PATIENT
+where
+    GEND_CD = "W" and
+    AGE <= 12
+order by
+    AGE desc,
+    PT_NAME asc;
