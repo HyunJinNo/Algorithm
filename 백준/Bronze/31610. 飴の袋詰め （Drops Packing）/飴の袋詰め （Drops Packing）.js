92ef071ba651bc33fa559ad70e108a6499a795d3
노현진 <nhj9831@naver.com>
2025-05-25 10:16:40 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 140 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.js
@@ -0,0 +1,7 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(path, "utf-8").toString().split("\n");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+const C = Number(input[2]);
+console.log(A * B + C);
