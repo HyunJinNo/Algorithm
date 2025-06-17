7e3f7737b872de6ba4b9f014c5af5287e6951e70
노현진 <nhj9831@naver.com>
2025-06-17 10:30:02 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 732 ms, Memory: 91572 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
@@ -0,0 +1,34 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(path, "utf-8").toString().split("\n");
+
+// N: 정점의 수, 5 <= N <= 100_000
+// M: 간선의 수, 1 <= M <= 200_000
+// R: 시작 정점, 1 <= R <= N
+const [N, M, R] = input[0].split(" ").map(Number);
+const graph = Array.from({ length: N + 1 }, () => []);
+
+for (let i = 1; i <= M; i++) {
+  const [u, v] = input[i].split(" ").map(Number);
+  graph[u].push(v);
+  graph[v].push(u);
+}
+
+for (let i = 1; i <= N; i++) {
+  graph[i].sort((a, b) => a - b);
+}
+
+const answer = Array(N + 1).fill(0);
+answer[R] = 1;
+let count = 2;
+
+const findAnswer = (u) => {
+  for (let v of graph[u]) {
+    if (answer[v] === 0) {
+      answer[v] = count++;
+      findAnswer(v);
+    }
+  }
+};
+
+findAnswer(R);
+console.log(answer.slice(1).join("\n"));
