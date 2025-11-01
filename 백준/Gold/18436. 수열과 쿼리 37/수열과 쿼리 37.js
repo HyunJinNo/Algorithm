3f0d22604a4296d44275e783c1d4206d2b441364
노현진 <nhj9831@naver.com>
2025-11-01 10:00:21 +0900

    [Gold I] Title: 수열과 쿼리 37, Time: 768 ms, Memory: 90276 KB -BaekjoonHub

---
+++ 백준/Gold/18436. 수열과 쿼리 37/수열과 쿼리 37.js
@@ -0,0 +1,75 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(path).toString().split("\n");
+
+const N = Number(input[0]); // 수열의 크기, 1 <= N <= 100_000
+const A = input[1].split(" ").map(Number); // 1 <= A[i] <= 1_000_000_000
+const M = Number(input[2]); // 쿼리의 개수, 1 <= M <= 100_000
+const tree = Array(N * 4);
+
+const init = (left, right, node) => {
+  if (left === right) {
+    if (A[left] % 2 === 0) {
+      tree[node] = [1, 0]; // [짝수 개수, 홀수 개수]
+    } else {
+      tree[node] = [0, 1]; // [짝수 개수, 홀수 개수]
+    }
+    return;
+  }
+
+  const mid = Math.floor((left + right) / 2);
+  init(left, mid, node * 2);
+  init(mid + 1, right, node * 2 + 1);
+  tree[node] = [tree[node * 2][0] + tree[node * 2 + 1][0], tree[node * 2][1] + tree[node * 2 + 1][1]];
+};
+
+const query = (left, right, node, nodeLeft, nodeRight) => {
+  if (right < nodeLeft || nodeRight < left) {
+    return [0, 0];
+  }
+
+  if (left <= nodeLeft && nodeRight <= right) {
+    return tree[node];
+  }
+
+  const mid = Math.floor((nodeLeft + nodeRight) / 2);
+  const leftResult = query(left, right, node * 2, nodeLeft, mid);
+  const rightResult = query(left, right, node * 2 + 1, mid + 1, nodeRight);
+  return [leftResult[0] + rightResult[0], leftResult[1] + rightResult[1]];
+};
+
+const update = (index, newValue, node, nodeLeft, nodeRight) => {
+  if (index < nodeLeft || nodeRight < index) {
+    return;
+  }
+
+  if (nodeLeft === nodeRight) {
+    if (newValue % 2 === 0) {
+      tree[node] = [1, 0];
+    } else {
+      tree[node] = [0, 1];
+    }
+    return;
+  }
+
+  const mid = Math.floor((nodeLeft + nodeRight) / 2);
+  update(index, newValue, node * 2, nodeLeft, mid);
+  update(index, newValue, node * 2 + 1, mid + 1, nodeRight);
+  tree[node] = [tree[node * 2][0] + tree[node * 2 + 1][0], tree[node * 2][1] + tree[node * 2 + 1][1]];
+};
+
+init(0, N - 1, 1);
+let answer = "";
+
+for (let i = 3; i < 3 + M; i++) {
+  const [a, b, c] = input[i].split(" ").map(Number);
+
+  if (a === 1) {
+    update(b - 1, c, 1, 0, N - 1);
+  } else if (a === 2) {
+    answer += `${query(b - 1, c - 1, 1, 0, N - 1)[0]}\n`;
+  } else {
+    answer += `${query(b - 1, c - 1, 1, 0, N - 1)[1]}\n`;
+  }
+}
+
+console.log(answer.trim());
