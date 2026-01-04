f530ba1eb76f686601c337568d6d47a680a25f66
노현진 <nhj9831@naver.com>
2026-01-04 10:52:20 +0900

    [Platinum V] Title: 피보나치 수와 최대공약수, Time: 148 ms, Memory: 9396 KB -BaekjoonHub

---
+++ 백준/Platinum/11778. 피보나치 수와 최대공약수/피보나치 수와 최대공약수.js
@@ -0,0 +1,43 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(path).toString().trim();
+
+const MOD = 1_000_000_007n;
+const [n, m] = input.split(" ").map(BigInt);
+const cache = new Map();
+
+/**
+ * @param {BigInt} n
+ */
+const fibo = (n) => {
+  if (n === 0n) {
+    return 0n;
+  } else if (n === 1n) {
+    return 1n;
+  } else if (n === 2n) {
+    return 1n;
+  } else if (cache.has(n)) {
+    return cache.get(n);
+  } else {
+    if (n % 2n === 1n) {
+      const m = (n + 1n) / 2n;
+      const value = (fibo(m) * fibo(m) + fibo(m - 1n) * fibo(m - 1n)) % MOD;
+      cache.set(n, value);
+      return value;
+    } else {
+      const m = n / 2n;
+      const value = ((2n * fibo(m - 1n) + fibo(m)) * fibo(m)) % MOD;
+      cache.set(n, value);
+      return value;
+    }
+  }
+};
+
+const gcd = (a, b) => {
+  if (b === 0n) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+};
+
+console.log(fibo(gcd(n, m)).toString());
