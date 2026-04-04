902095315c74771fe101e02ad233d68be6bf966a
노현진 <nhj9831@naver.com>
2026-04-04 11:26:01 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 616 ms, Memory: 76016 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.js
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.js
@@ -4,7 +4,7 @@ const input = require("fs").readFileSync(path, "utf-8").toString().split("\n");
 // N: 멜로디에 포함되어 있는 음의 수, 1 <= N <= 500_000
 // P: 한 줄에 있는 프렛의 수, 2 <= P <= 300_000
 const [N, P] = input[0].split(" ").map(Number);
-const stackList = Array.from({ length: N + 1 }, () => []);
+const stackList = Array.from({ length: 7 }, () => []);
 let answer = 0;
 
 for (let i = 1; i <= N; i++) {
