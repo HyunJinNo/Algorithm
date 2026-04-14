65bac72c1562c45f12c46c55832730f0c191e251
노현진 <nhj9831@naver.com>
2026-04-14 10:59:47 +0900

    [Unrated] Title: BASE64 인코딩, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Unrated/10935. BASE64 인코딩/BASE64 인코딩.js
@@ -0,0 +1,5 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(path, "utf-8").toString();
+const S = input.trim();
+
+console.log(btoa(S));
