259bc40ef0c11acb1b93ae1d27f0696ea518c938
노현진 <nhj9831@naver.com>
2026-06-15 10:18:07 +0900

    Time: 1 ms (72.13%), Space: 58.1 MB (31.74%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let answer = 0;
+
+    for (let num of nums) {
+        answer ^= num;
+    }
+
+    return answer;
+};
