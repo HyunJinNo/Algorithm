9409ab1611bb069b9659778fbd2b04481bc71286
노현진 <nhj9831@naver.com>
2026-06-21 10:22:36 +0900

    Time: 1 ms (74.2%), Space: 57.3 MB (16.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,6 +2,23 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x));
+var mySqrt = function (x) {
+    if (x === 0) {
+        return 0;
+    }
+
+    let left = 1;
+    let right = x;
+
+    while (left + 1 < right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (mid * mid > x) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+
+    return left;
 };
