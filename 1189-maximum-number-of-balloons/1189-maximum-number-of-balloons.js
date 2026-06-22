a9c666ded96b6efc6b88002ca62df5bcf395d3b4
노현진 <nhj9831@naver.com>
2026-06-22 09:59:29 +0900

    Time: 8 ms (40.77%), Space: 58.8 MB (10.77%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -3,20 +3,21 @@
  * @return {number}
  */
 var maxNumberOfBalloons = function (text) {
-    let answer = Number.MAX_SAFE_INTEGER;
-    const map = new Map();
+    const arr = [0, 0, 0, 0, 0];
 
     for (let c of text) {
-        map.set(c, (map.get(c) ?? 0) + 1);
+        if (c === "b") {
+            arr[0]++;
+        } else if (c === "a") {
+            arr[1]++;
+        } else if (c === "l") {
+            arr[2]++;
+        } else if (c === "o") {
+            arr[3]++;
+        } else if (c === "n") {
+            arr[4]++;
+        }
     }
 
-    answer = Math.min(
-        answer,
-        map.get("b") ?? 0,
-        map.get("a") ?? 0,
-        Math.floor((map.get("l") ?? 0) / 2),
-        Math.floor((map.get("o") ?? 0) / 2),
-        map.get("n") ?? 0);
-
-    return answer;
+    return Math.min(arr[0], arr[1], Math.floor(arr[2] / 2), Math.floor(arr[3] / 2), arr[4]);
 };
