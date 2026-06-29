b411f8ac16a462d906ab855af6228c2ad21216b8
노현진 <nhj9831@naver.com>
2026-06-29 09:41:13 +0900

    Time: 0 ms (100%), Space: 54.2 MB (36.11%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function(patterns, word) {
+    let count = 0;
+
+    for (let pattern of patterns) {
+        if (word.indexOf(pattern) !== -1) {
+            count++;
+        }
+    }
+
+    return count;
+};
