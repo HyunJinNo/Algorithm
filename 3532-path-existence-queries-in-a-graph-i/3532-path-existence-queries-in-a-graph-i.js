b643780fdce69dcd2a1894bd2d6191084c9ac863
노현진 <nhj9831@naver.com>
2026-07-09 10:55:27 +0900

    Time: 29 ms (63.16%), Space: 88.8 MB (36.84%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.js
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.js
@@ -6,45 +6,20 @@
  * @return {boolean[]}
  */
 var pathExistenceQueries = function (n, nums, maxDiff, queries) {
-    const parent = Array.from({ length: n }, (_value, index) => index);
-    const rank = Array(n).fill(0);
-
-    const find = (x) => {
-        if (parent[x] !== x) {
-            parent[x] = find(parent[x]);
-        }
-
-        return parent[x];
-    }
-
-    const union = (x, y) => {
-        const rootX = find(x);
-        const rootY = find(y);
-
-        if (rootX === rootY) {
-            return;
-        }
-
-        if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else if (rank[rootX] > rank[rootY]) {
-            parent[rootY] = rootX;
-        } else {
-            parent[rootY] = rootX;
-            rank[rootX]++;
-        }
-    }
-
+    const parent = Array(n).fill(0);
+    
     for (let i = 0; i < n - 1; i++) {
         if (nums[i + 1] - nums[i] <= maxDiff) {
-            union(i, i + 1);
+            parent[i + 1] = parent[i];
+        } else {
+            parent[i + 1] = parent[i] + 1;
         }
     }
 
     const result = [];
 
     for (const [u, v] of queries) {
-        result.push(find(u) === find(v));
+        result.push(parent[u] === parent[v]);
     }
 
     return result;
